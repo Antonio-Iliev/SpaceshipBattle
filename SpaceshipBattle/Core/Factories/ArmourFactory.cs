3741bbca5b137f4e372186b3c48322198dc26dcd
Antonio Iliev <iliev.t.a@gmail.com>
2018-08-07 16:14:44 +0300

    Change Armor names.

--- SpaceshipBattle/Core/Factories/ArmourFactory.cs
+++ SpaceshipBattle/Core/Factories/ArmourFactory.cs
@@ -12,20 +12,20 @@ namespace SpaceshipBattle.Core.Factories
         {
             switch (model)
             {
-                case "Paper":
+                case "Recycled Paper":
                     return new DenseArmour("Recycled Paper", 1000, 50, 20, 0, 2, ArmourType.Paper);
                 case "Brick cage":
                     return new DenseArmour("Brick cage", 1500, 800, 25, 6, 0, ArmourType.Brick);
-                case "Aerogel":
-                    return new DenseArmour("Aerogel Armour", 2000, 500, 30, 2, 5, ArmourType.Aerogel);
-                case "Fullerenes":
+                case "Aerogel cover":
+                    return new DenseArmour("Aerogel cover", 2000, 500, 30, 2, 5, ArmourType.Aerogel);
+                case "Fullerenes Armour":
                     return new DenseArmour("Fullerenes Armour", 3500, 200, 40, 7, 3, ArmourType.Carbon);
-                case "Switz":
+                case "Switz Armour":
                     return new DenseArmour("Switz Armour", 2500, 2500, 35, 3, 2, ArmourType.Steel);
-                case "Plasma":
-                    return new ProtectiveFields("Plasma Armour", 1800, 100, 35, 2, 8, ArmourType.Plasma);
-                case "AntiMatter":
-                    return new ProtectiveFields("Anti Matter Fields", 5000, 450, 65, 0, 10, ArmourType.AntiMatter);
+                case "Plasma Field":
+                    return new ProtectiveFields("Plasma Field", 1800, 100, 35, 2, 8, ArmourType.Plasma);
+                case "Anti Matter Fields":
+                    return new ProtectiveFields("Anti Matter Field", 5000, 450, 65, 0, 10, ArmourType.AntiMatter);
                 default:
                     throw new ArgumentException("There is no such a model!");
             }
