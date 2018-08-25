5d3ee0df9dea07c1cb3a42184a6b0fc3fb48ab84
Antonio Iliev <iliev.t.a@gmail.com>
2018-08-25 12:15:08 +0300

    - Split container registration in methods.

--- SpaceshipBattle/Injector/ConteinerInjector.cs
+++ SpaceshipBattle/Injector/ConteinerInjector.cs
@@ -1,4 +1,5 @@
-﻿using Autofac;
+﻿using System.Reflection;
+using Autofac;
 using SpaceshipBattle.Contracts;
 using SpaceshipBattle.Contracts.Factories;
 using SpaceshipBattle.Contracts.Providers;
@@ -7,32 +8,44 @@ using SpaceshipBattle.Core.Factories;
 using SpaceshipBattle.Core.Providers;
 using System;
 using System.Collections.Generic;
+
 using System.Text;
 
 namespace SpaceshipBattle.Injector
 {
-    class ConteinerInjector : Module
+    class ConteinerInjector : Autofac.Module
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<Engine>().As<IEngine>();
+            this.RegisterFactoryComponents(builder);
+            this.RegisterCoreComponents(builder);
+            this.RegisterProvidersComponents(builder);
 
+            base.Load(builder);
+        }
+
+        private void RegisterFactoryComponents(ContainerBuilder builder)
+        {
             builder.RegisterType<EngineFactory>().As<IEngineFactory>();
             builder.RegisterType<ArmourFactory>().As<IArmourFactory>();
             builder.RegisterType<WeaponFactory>().As<IWeaponFactory>();
             builder.RegisterType<SpaceshipFactory>().As<ISpaceshipFactory>();
             builder.RegisterType<BulletFactory>().As<IBulletFactory>();
             builder.RegisterType<PlayerFactory>().As<IPlayerFactory>();
+        }
 
+        private void RegisterCoreComponents(ContainerBuilder builder)
+        {
+            builder.RegisterType<Engine>().As<IEngine>();
             builder.RegisterType<Registration>().As<IRegistration>();
             builder.RegisterType<PlayerCreator>().As<IPlayerCreator>();
             builder.RegisterType<GameController>().As<IGameController>();
+        }
 
+        private void RegisterProvidersComponents(ContainerBuilder builder)
+        {
             builder.RegisterType<ConsoleWriter>().As<IWriter>();
             builder.RegisterType<ConsoleReader>().As<IReader>();
-
-
-            base.Load(builder);
         }
     }
 }
--- SpaceshipBattle/StartUp.cs
+++ SpaceshipBattle/StartUp.cs
@@ -21,26 +21,6 @@ namespace SpaceshipBattle
 
             engine.Start();
 
-
-            //IEngineFactory engineFactory = new EngineFactory();
-            //IArmourFactory armourFactory = new ArmourFactory();
-            //IWeaponFactory weaponFactory = new WeaponFactory();
-            //ISpaceshipFactory spaceshipFactory = new SpaceshipFactory();
-            //IBulletFactory bulletFactory = new BulletFactory();
-            //IPlayerFactory playerFactory = new PlayerFactory();
-            
-            //IWriter writer = new ConsoleWriter();
-            //IConsoleReader reader = new ConsoleReader();
-
-            //PlayerCreator playerCreator = new PlayerCreator(playerFactory, engineFactory, armourFactory, weaponFactory, spaceshipFactory, bulletFactory);
-            //GameController gameController = new GameController(writer, reader);
-            //Registration player1 = new Registration(reader, writer);
-            //Registration player2 = new Registration(reader, writer);
-
-            //var engine = new Engine(playerCreator, gameController, player1, player2, writer, reader);
-            //engine.Start();
-
-
         }
 
     }
