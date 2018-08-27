772ecd94ef202d14463a64730519295c6d16ca63
Alexander Stoychev <stoychev92@yahoo.co.uk>
2018-08-28 00:42:31 +0300

    Added AreaOfEffectWeaponTests

--- SpaceshipBattle.Tests/WeaponTests/AreaOfEffectWeaponTests/CreateAreaOfEffectWeapon_Should.cs
+++ SpaceshipBattle.Tests/WeaponTests/AreaOfEffectWeaponTests/CreateAreaOfEffectWeapon_Should.cs
@@ -1,22 +1,22 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpaceshipBattle.Contracts.Entities.Weapon;
 using SpaceshipBattle.Entities.Weapons;
 using SpaceshipBattle.Tests.WeaponAbstractTest.Mock;
 using System;
-
-namespace SpaceshipBattle.Tests.WeaponTests.CriticalStrikeWeaponTests
+namespace SpaceshipBattle.Tests.WeaponTests.AreaOfEffectWeaponTests
 {
     [TestClass]
-    public class CreateCriticalStrikeWeapon_Should
+    public class CreateAreaOfEffectWeapon_Should
     {
         [TestMethod]
-        public void CreateCriticalStrikeWeapon_WhenMethodIsCalled()
+        public void CreateAreaOfEffectWeapon_WhenMethodIsCalled()
         {
             //Arrange
-            var engine = new CriticalStrikeWeapon("AK47", 2000, 20, 8, 4, 200, 30);
-            
+            var engine = new AreaOfEffectWeapon("Cannon", 2000, 20, 8, 4, 200, 30);
+
             //Assert
             Assert.IsNotNull(engine);
-        }        
+        }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
@@ -24,20 +24,20 @@ namespace SpaceshipBattle.Tests.WeaponTests.CriticalStrikeWeaponTests
         public void ThrowError_WhenWeightIsOutOfRange(int value)
         {
             //Arrange
-            var weapon = new CriticalStrikeWeapon("AK47", 1300, value,22 , 800,22, 100);
+            var weapon = new AreaOfEffectWeapon("AK47", 1300, value, 22, 800, 22, 100);
         }
 
         [TestMethod]
         public void ReturnProperMessage_WhenToStringIsCalled()
         {
             //Arrange
-            var engine = new CriticalStrikeWeapon("AK47", 1300, 55, 22, 800, 22, 100);
+            var engine = new AreaOfEffectWeapon("Cannon", 1300, 55, 22, 800, 22, 100);
 
             //Act
             var result = engine.ToString();
 
             //Assert
-            StringAssert.Contains(result, "Model: AK47"
+            StringAssert.Contains(result, "Model: Cannon"
                 + Environment.NewLine + "Price: 1300$"
                 + Environment.NewLine + "Weight: 55kg");
         }
---
+++ SpaceshipBattle.Tests/WeaponTests/WeaponAbstract_Should.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpaceshipBattle.Tests.WeaponAbstractTest.Mock;
+
+namespace SpaceshipBattle.Tests.WeaponTests
+{
+    [TestClass]
+    public class WeaponAbstract_Should
+    {
+        [TestMethod]
+        [DataRow(11)]
+        [DataRow(110)]
+        [DataRow(240)]
+        public void ReturnCorrectValue_WhenPowerIsSet(int value)
+        {
+            //Arrange
+            var weapon = new WeaponAbstractMock("AK47", 1300, 222, value, 800, 100);
+
+            //Act & Assert
+            Assert.AreEqual(weapon.Power, value);
+        }
+    }
+}
